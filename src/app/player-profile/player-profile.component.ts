36727730b7b4b60d8fc8b2b06887230749f51f74
Vardan <asatrian.v@gmail.com>
2019-05-25 22:30:54 +0300

    Add create player form

--- src/app/player-profile/player-profile.component.ts
+++ src/app/player-profile/player-profile.component.ts
@@ -1,38 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { BrowserModule } from '@angular/platform-browser';
 import { PlayerService } from '../services/player.service';
-import { User } from '../interfaces/user';
 import { Player } from '../interfaces/player';
-import { FormBuilder, Validators, FormGroup, PatternValidator, FormControl} from '@angular/forms';
-import { IdItem } from '../interfaces/id-item';
-import {  dropdownListRanksConfig,
-          dropdownSettingsRanksConfig,
-          dropdownListGoalsConfig,
-          dropdownSettingsGoalsConfig,
-          dropdownListServersConfig,
-          dropdownSettingsServersConfig,
-          dropdownSettingsGamesConfig,
-          dropdownListGamesConfig
-        } from '../services/dataconfig';
+import { FormBuilder, Validators } from '@angular/forms';
+import {
+  dropdownListRanksConfig,
+  dropdownSettingsRanksConfig,
+  dropdownListGoalsConfig,
+  dropdownSettingsGoalsConfig,
+  dropdownListServersConfig,
+  dropdownSettingsServersConfig,
+  dropdownSettingsGamesConfig,
+  dropdownListGamesConfig
+} from '../services/dataconfig';
 import { ToastrService } from 'ngx-toastr';
+import { PlayerCommunicationServer } from '../services/player.communication.service';
 
 @Component({
   selector: 'app-player-profile',
   templateUrl: './player-profile.component.html',
   styleUrls: ['./player-profile.component.scss']
 })
+
 export class PlayerProfileComponent implements OnInit {
 
   constructor(
     private service: PlayerService,
     private fb: FormBuilder,
-    private toastr: ToastrService) { }
+    private toastr: ToastrService,
+    private data: PlayerCommunicationServer) { }
 
-  public createButton = true;
-  private playerProfile = false;
-
-  players: Player[] = [{
+  player: Player = {
     id: 0,
     name: '',
     game: '',
@@ -40,7 +37,7 @@ export class PlayerProfileComponent implements OnInit {
     goal: '',
     about: '',
     rank: ''
-  }];
+  };
 
   formModelPlayer = this.fb.group({
     name: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(16)]],
@@ -50,44 +47,28 @@ export class PlayerProfileComponent implements OnInit {
     about: [''],
     rank: ['', Validators.required]
   });
-  
+
   //Multiselect-dropdown - Game
   dropdownListGame = [];
-  selectedItemsGame = '';
   dropdownSettingsGame = {};
-  
+
   //Multiselect-dropdown - Server
   dropdownListServer = [];
-  selectedItemsServer = '';
   dropdownSettingsServer = {};
-  
+
   //Multiselect-dropdown - Goal
   dropdownListGoal = [];
-  selectedItemsGoal = '';
   dropdownSettingsGoal = {};
-  
+
   //Multiselect-dropdown - Rank
   dropdownListRank = [];
-  selectedItemsRank =  '';
   dropdownSettingsRank = {};
-  
+
   ngOnInit() {
-    this.service.getPlayersOfUser(1).subscribe(
-      res => {
-        if (res.length === 0) {
-          this.createButton = true;
-          this.playerProfile = false;
-        }
-        this.createButton = false;
-        this.playerProfile = true;
-        this.players = res;
-        this.setCurrentUserInfo();
-      }
-    );
     this.initializeDefaultConfig();
   }
 
-  onSubmit(){
+  onSubmit() {
     this.service.createOrUpdatePlayer(this.formModelPlayer.value).subscribe(
       res => {
         this.toastr.success(res.info, res.message);
@@ -97,6 +78,11 @@ export class PlayerProfileComponent implements OnInit {
       }
     );
 
+  }
+
+  cancel() {
+    this.data.changeCreatePlayer(false);
+    this.data.changeListOfPlayers(true);
   }
 
   private initializeDefaultConfig() {
@@ -112,21 +98,4 @@ export class PlayerProfileComponent implements OnInit {
     this.dropdownListRank = dropdownListRanksConfig;
     this.dropdownSettingsRank = dropdownSettingsRanksConfig;
   }
-
-  createPlayer(){
-    this.createButton = false;
-    this.playerProfile = true;
-  }
-
-  performDelete() {
-    //this._modalService.open(ConfirmDeleteComponent);
-  }
-
-  private setCurrentUserInfo() {
-    this.selectedItemsGame = (this.players[0].game);
-    this.selectedItemsServer = (this.players[0].server);
-    this.selectedItemsGoal = (this.players[0].goal);
-    this.selectedItemsRank = (this.players[0].rank);
-  }
-
 }
