74e56f1b0f118d2cceb779ff51581c1b986c0ab6
vardan <asatrian.v@gmail.com>
2019-06-05 20:07:54 +0300

    Markup player page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -20,6 +20,7 @@ import { DotaComponent } from './global-players/dota/dota.component';
 import { GtaComponent } from './global-players/gta/gta.component';
 import { CsGoComponent } from './global-players/cs-go/cs-go.component';
 import {RestoreComponent} from './account/restore/restore.component';
+import { PlayerPageComponent } from './player/player-page/player-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full'},
@@ -50,9 +51,10 @@ const routes: Routes = [
       { path: '**', component: NotFoundComponent }
     ]
 },
-    
+
 
   { path: 'userprofile', component: UserProfileComponent },
+  { path: 'player', component: PlayerPageComponent },
 
   {
     path:'players',
--- src/app/player/player-page/player-page.component.ts
+++ src/app/player/player-page/player-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Player } from 'src/app/interfaces/player';
 
 @Component({
   selector: 'app-player-page',
@@ -9,7 +10,44 @@ export class PlayerPageComponent implements OnInit {
 
   constructor() { }
 
+  isAvailable: true;
+
+  player: Player = {
+    id: 0,
+    name: 'playerName',
+    game: '',
+    server: 'playerServer',
+    goal: 'playerGoal',
+    about: 'PlayerAbout',
+    rank: 'playerRank',
+    decency: 1
+  };
+
+  // id
+  // name
+  // server
+  // goal
+  // rank
+  // decency
+  // about
+  playerPhoto: 'https://pbs.twimg.com/profile_images/900338165113815045/aA0Wx0uR_400x400.jpg';
+  age: 18;
+  languages: ['playerlanguage1', 'playerlanguage2'];
+  country: 'playerCountry';
+  teamId: 1;
+  teamName: 'teamName';
+  teamPhoto: 'http://localhost:5000/api/Team/Logo/12e4ead2-d52f-4d7f-b796-4b524026fc64';
+
+
   ngOnInit() {
+    this.playerPhoto = 'https://pbs.twimg.com/profile_images/900338165113815045/aA0Wx0uR_400x400.jpg';
+    this.age = 18;
+    this.languages = ['playerlanguage1', 'playerlanguage2'];
+    this.country = 'playerCountry';
+    this.teamId = 1;
+    this.teamName = 'teamName';
+    this.teamPhoto = 'http://localhost:5000/api/Team/Logo/12e4ead2-d52f-4d7f-b796-4b524026fc64';
+
   }
 
 }
